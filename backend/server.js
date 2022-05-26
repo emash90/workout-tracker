8e2db1a4288b6af16b6ef73b099d43c966ba3f00
edwin <emash90@gmail.com>
2022-05-26 16:45:54 +0300

    fixing the / route

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/exercise', require('./routes/exerciseRoute'))
 
+if(process.env.NODE_ENV === 'production'){app.use(express.static(path.join(__dirname, '../frontend/build')))} else {
+    app.get('/', (req, res) => {
+        res.send('set to "production"')
+    })
+}
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 
 
